c0c25e1842fb6d4d57ac21eda83288a0fd46ac55
evanhwz <evan.haryo@ui.ac.id>
2026-05-22 19:50:23 +0700

    add monitoring config

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 }
 
 dependencyManagement {
